112ac857466d2690e65556f5fb2a114ba95e19ab
Arthur Almeida <arthurmeirelesalmeida@gmail.com>
2020-12-17 00:39:30 -0300

    Acrescentada a mensagem desejando um bom dia

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -4,8 +4,8 @@ namespace Teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Olá mundo!");
-            Console.WriteLine("Bom dia!");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia!");
 
             Console.ReadLine();
         }
